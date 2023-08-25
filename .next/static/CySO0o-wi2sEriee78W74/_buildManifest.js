99f4803dc5903ced0284510b6f5c20786f62924e
battuya.e <battuya.e@mobicom.mn>
2023-08-25 10:01:23 +0800

    build project

---
+++ .next/static/CySO0o-wi2sEriee78W74/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-6d96b60ad4936660.js","static/chunks/pages/index-a06f6cc5444c46f4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

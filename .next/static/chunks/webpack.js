35b3ac72c627b688f419c603371884ef74f67e3c
Amgalan Gerelsaikhan <amgalangerelsaikhan@Amgalans-MacBook-Pro.local>
2023-09-07 22:01:52 +0900

    added footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ff22026eed0ac8e"; }
+/******/ 		__webpack_require__.h = function() { return "3e70fb138282b8b3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,49 @@
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
-    const { t } = useTranslation("");
+  const { t } = useTranslation("");
 
-    return (
-        <footer className='w-full h-48 bg-gray-300 text-black-100'>
-            <div className='sm:px-16 px-6 py-10'>
-                <div className='flex flex-col justify-start items-start gap-6'>
-                    <img src="/dotaij-logo.png" alt="logo" width={120} height={30} className='object-contain' />
-                    <p className='text-base text-gray-700'>{t('footer.title')}</p>
-                </div>
-                <div className=''>
-                    {t('footer.copyright')} ©
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-gray-300 py-6">
+      <div className="container mx-auto flex flex-col items-center">
+        {/* Menu Navigation */}
+        <nav className="mb-4">
+          <ul className="flex gap-4">
+            <li>
+              <Link href="/book">Books</Link>
+            </li>
+            <li>
+              <Link href="/biography">Biography</Link>
+            </li>
+            <li>
+              <Link href="/events">Events</Link>
+            </li>
+            <li>
+              <Link href="/contact">Events</Link>
+            </li></ul>
+        </nav>
+
+        {/* Centered Social Media Icons */}
+        <div className="flex gap-4">
+          {/* Add your social media icons and links here */}
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <FaFacebook size={20} style={{ color: "black" }} />
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <FaTwitter size={20} style={{ color: "black" }} />
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <FaInstagram size={20} style={{ color: "black" }} />
+          </a>
+        </div>
+
+        {/* Copyright Message */}
+        <div className="text-center text-gray-700 text-xs mt-4">
+          {t('footer.copyright')} © 2000 - 2024 Do Taij Mogul
+        </div>
+      </div>
+    </footer>
+  );
 }

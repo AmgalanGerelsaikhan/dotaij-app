8d1edd2aa4f075b1ab5de13ec042cd23b33ec92c
Amgalan Gerelsaikhan <gerel.amgaa@gmail.com>
2023-09-10 12:14:07 +0900

    Navigation

--- components/my-navigation.js
+++ components/my-navigation.js
@@ -5,86 +5,92 @@ import { useTranslation } from "next-i18next";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Menu from "./menu";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 
 export default function Navigation() {
-    const [navbar, setNavbar] = useState(false)
-    const router = useRouter();
+  const [navbar, setNavbar] = useState(false);
+  const router = useRouter();
 
-    const { t } = useTranslation("");
+  const { t } = useTranslation("");
 
-    useEffect(() => {
-        let dir = "ltr";
-        let lang = router.locale == "uk" ? "uk" : "en";
-        document.querySelector("html").setAttribute("dir", dir);
-        document.querySelector("html").setAttribute("lang", lang);
-    }, [router.locale]);
+  useEffect(() => {
+    let dir = "ltr";
+    let lang = router.locale == "uk" ? "uk" : "en";
+    document.querySelector("html").setAttribute("dir", dir);
+    document.querySelector("html").setAttribute("lang", lang);
+  }, [router.locale]);
 
-    return (
-        <nav className="w-full">
-            <div className=" bg-[#2d323d] text-[#98a2aa] px-5 py-2 flex">
+  return (
+    <nav className="w-full">
+      <div className="bg-[#2d323d] text-[#98a2aa] px-5 py-2 flex">
+        <div className="flex-1 justify-start">
+          <div className="px-5">
+            <div className="flex justify-between md:py-1">
+              {/* LOGO */}
+              <div className="flex items-center">
+                <Link href="/" className="">
+                  <div className="group relative">
+                    <img
+                      src="/LOGO.png"
+                      alt="logo"
+                      width={130}
+                      height={10}
+                      className="object-contain transition-transform transform group-hover:scale-110"
+                    />
+                    <div className="absolute inset-0 opacity-0 group-hover:opacity-80 transition-opacity"></div>
+                  </div>
+                </Link>
+              </div>
 
-                <div className="flex-1 justify-start">
-
-                    <div className="px-5">
-                        <div className="flex justify-between md:py-1 ">
-
-                            {/* LOGO */}
-                            <div className="">
-                                <Link href="/" className="">
-                                    <img src="/dotaij-logo.png" alt="logo" width={180} height={20} className="object-contain" />
-                                </Link>
-                            </div>
-
-                            <div className={`md:block ${navbar ? 'block' : 'hidden'}`}>
-                                <div className="md:h-auto md:flex cursor-pointer md:mt-1 mt-8">
-                                    <Link href="/book" onClick={() => setNavbar(!navbar)}>
-                                        <p className="text-xl px-6 text-center py-5 md:py-2 md:hover:bg-transparent hover:font-bold">
-                                            {t('header.Books')}
-                                        </p>
-                                    </Link>
-                                    <Link href="/biography" onClick={() => setNavbar(!navbar)}>
-                                        <p className="text-xl px-6 text-center py-5 md:py-2 md:hover:bg-transparent hover:font-bold">
-                                            {t('header.Biography')}
-                                        </p>
-                                    </Link>
-                                    <Link href="/events" onClick={() => setNavbar(!navbar)}>
-                                        <p className="text-xl px-6 text-center py-5 md:py-2 md:hover:bg-transparent hover:font-bold">
-                                            {t('header.Events')}
-                                        </p>
-                                    </Link>
-                                    <Link href="/contact" onClick={() => setNavbar(!navbar)}>
-                                        <p className="text-xl px-6 text-center py-5 md:py-2 md:hover:bg-transparent hover:font-bold">
-                                            {t('header.Contact')}
-                                        </p>
-                                    </Link>
-
-                                </div>
-                            </div>
-
-                            {/* MOBILE */}
-                            <div className="md:hidden py-0">
-                                <button className="py-3 rounded-md" onClick={() => setNavbar(!navbar)}>
-                                    {navbar ? (
-                                        <Image src="/close.svg" width={30} height={30} alt="close icon button" />
-                                        // <Image src="/close_gray.png" width={30} height={30} alt="close icon button" />
-                                    ) : (
-                                        <Image src="/menu.svg" width={30} height={30} alt="menu icon button" />
-                                        // <Image src="/menu_gray.png" width={30} height={30} alt="menu icon button" />
-                                    )}
-                                </button>
-                            </div>
-
-                        </div>
-
-                    </div>
-                </div >
-
-                <div className="flex-none">
-                    <div className="justify-end mt-3">
-                        <LocaleSwitcher />
-                    </div>
+              <div className={`md:block ${navbar ? "block" : "hidden"}`}>
+                {/* Center-align menu items */}
+                <div className="md:h-auto md:flex justify-center cursor-pointer md:mt-1 mt-4"> 
+                  <Link href="/book" onClick={() => setNavbar(!navbar)}>
+                    <p className="text-lg px-4 py-3 md:py-2 md:hover:bg-transparent hover:font-bold"> 
+                      {t("header.Books")}
+                    </p>
+                  </Link>
+                  <Link href="/biography" onClick={() => setNavbar(!navbar)}>
+                    <p className="text-lg px-4 py-3 md:py-2 md:hover:bg-transparent hover:font-bold"> 
+                      {t("header.Biography")}
+                    </p>
+                  </Link>
+                  <Link href="/events" onClick={() => setNavbar(!navbar)}>
+                    <p className="text-lg px-4 py-3 md:py-2 md:hover:bg-transparent hover:font-bold"> 
+                      {t("header.Events")}
+                    </p>
+                  </Link>
+                  <Link href="/contact" onClick={() => setNavbar(!navbar)}>
+                    <p className="text-lg px-4 py-3 md:py-2 md:hover:bg-transparent hover:font-bold"> 
+                      {t("header.Contact")}
+                    </p>
+                  </Link>
                 </div>
-            </div >
-        </nav>
-    );
+              </div>
+
+              {/* MOBILE */}
+              <div className="md:hidden py-0">
+                <button
+                  className="py-3 rounded-md"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <HiOutlineX size={30} />
+                  ) : (
+                    <HiOutlineMenu size={30} />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-none">
+          <div className="justify-end mt-2"> {/* Reduced margin-top */}
+            <LocaleSwitcher />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
